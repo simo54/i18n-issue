c89f302382704a489f05dacde1b6ec9e39b2ef67
scava <sorzesimone1@gmail.com>
2022-06-03 19:04:11 +0200

    fix language detector

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import detector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
+import XHR from 'i18next-http-backend';
 
 enum Languages {
 	IT = 'it',
@@ -38,11 +39,11 @@ const optionsLan = {
 
 if (!i18n.isInitialized) {
 	i18n.use(Backend)
-		.use(detector)
+		.use(XHR)
+		.use(LanguageDetector)
 		.use(initReactI18next)
 		.init({
 			detection: optionsLan,
-			lng: 'it',
 			fallbackLng,
 			debug: process.env.NODE_ENV === 'development',
 			interpolation: {
