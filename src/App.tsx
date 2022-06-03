75b6ba85cee2806b06fa7a37ddd27ebd9364fcd2
scava <sorzesimone1@gmail.com>
2022-06-03 18:30:20 +0200

    code test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { t } = useTranslation(['commons']);
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>{t('commons.hello')}</p>
+				<p>{t('commons.longtexttest')}</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
