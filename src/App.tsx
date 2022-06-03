89e9e94bf3113edd080556757beb457e82fc0850
scava <sorzesimone1@gmail.com>
2022-06-03 18:33:48 +0200

    fix translation

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>{t('commons.hello')}</p>
-				<p>{t('commons.longtexttest')}</p>
+				<p>{t('commons:hello')}</p>
+				<p>{t('commons:longtexttest')}</p>
 			</header>
 		</div>
 	);
